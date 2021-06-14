c704b7132ae4032000425ae2b6092bc18f600592
dotslashed <73475291+dotslashed@users.noreply.github.com>
2021-06-14 16:42:12 +0530

    Update domcrt.sh

--- domcrt.sh
+++ domcrt.sh
@@ -9,3 +9,7 @@ echo "[+]Calculating the subdomains..."
 sleep 5
 
 curl -sk "https://crt.sh/?q=$DOMAIN" | grep -oE "[a-zA-Z0-9._-]+\.$DOMAIN" | sed -e '/[A-Z]/d' -e '/*/d' | grep -oP '[a-z0-9]+\.[a-z]+\.[a-z]+'
+
+#ALTERNATE WAY:
+
+#curl -s https://crt.sh/\?q=%25.target.com\&output\=json | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | tee file_name.txt
